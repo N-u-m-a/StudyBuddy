2ef5366ce46574e74caeb388a9db324807db25b6
mahbuba208 <20230657098@juniv.edu>
2026-08-23 14:53:03 +0600

    Update studentRoutes.js

--- backend/routes/studentRoutes.js
+++ backend/routes/studentRoutes.js
@@ -1,44 +1,18 @@
-// const express = require("express");
-// const router = express.Router();
-// const {
-//   createStudent,
-//   getAllStudents,
-//   getStudentById,
-//   updateStudent,
-//   deleteStudent,
-// } = require("../controllers/studentController"); // ensure controller path matches
-
-// // CREATE
-// router.post("/", createStudent);
-
-// // READ ALL
-// router.get("/", getAllStudents);
-
-// // READ BY ID
-// router.get("/:id", getStudentById);
-
-// // UPDATE
-// router.put("/:id", updateStudent);
-
-// // DELETE
-// router.delete("/:id", deleteStudent);
-
-// module.exports = router;
 const express = require("express");
 const router = express.Router();
 const {
-  createStudent,
-  getAllStudents,
+  getStudents,
   getStudentById,
+  createStudent,
   updateStudent,
   deleteStudent,
-} = require("../controllers/studentController"); // ensure controller path matches
+} = require("../controllers/studentController");
 
 // CREATE
 router.post("/", createStudent);
 
 // READ ALL
-router.get("/", getAllStudents);
+router.get("/", getStudents);
 
 // READ BY ID
 router.get("/:id", getStudentById);
